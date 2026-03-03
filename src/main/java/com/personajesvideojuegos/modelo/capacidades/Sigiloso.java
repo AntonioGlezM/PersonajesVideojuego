e39312ebb0de21bbf81620b97a808b92e93caba0
Gabriel Francisco Ruíz Bolaños <gabrielfranciscosi45@gmail.com>
2026-03-03 23:12:21 +0000

    Movida la clase Conjuro hacia Acciones por coherencia, creada la Accion Ocultacion, creadas interfaces cuyos métodos devuelven estas acciones, en el paquete capacidades.

---
+++ src/main/java/com/personajesvideojuegos/modelo/capacidades/Sigiloso.java
@@ -0,0 +1,7 @@
+package com.personajesvideojuegos.modelo.capacidades;
+
+import com.personajesvideojuegos.modelo.Acciones.Ocultacion;
+
+public interface Sigiloso {
+    public Ocultacion Ocultarse();
+}
--- src/main/java/com/personajesvideojuegos/modelo/conjuross/TipoConjuroSupport.java
+++ src/main/java/com/personajesvideojuegos/modelo/conjuross/TipoConjuroSupport.java
@@ -1,5 +1,8 @@
 package com.personajesvideojuegos.modelo.conjuross;
 
+/**
+ * @author Gabriel Francisco Ruiz Bolaños
+ * */
 public enum TipoConjuroSupport {
     DEFENSA,
     FUERZA,
