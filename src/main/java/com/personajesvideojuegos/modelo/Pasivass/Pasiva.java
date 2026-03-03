99441f0aeb263f4f8d0aa66269291945b6bd625a
alonsojrodrbetan-crypto <alonso.j.rodr.betan@gmail.com>
2026-03-03 19:42:52 +0000

    He metido lo que hace las pasivas en si, más las razas

--- src/main/java/com/personajesvideojuegos/modelo/Pasivass/Pasiva.java
+++ src/main/java/com/personajesvideojuegos/modelo/Pasivass/Pasiva.java
@@ -1,15 +1,28 @@
 package com.personajesvideojuegos.modelo.Pasivass;
 
 import com.personajesvideojuegos.modelo.Rasgo;
+
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * @author Alonso J rodríguez Betancor*/
 public abstract class Pasiva implements Rasgo {
     private String ID;
     private String titulo;
     private String descrip;
+    private List<Pasiva> clasePasivas = new ArrayList<>();
     public Pasiva(String id, String Titulo, String Descripcion){
         this.ID = id;
         this.titulo = Titulo;
         this.descrip = Descripcion;
+    }
+
+    public void agregarclasePasiva(Pasiva p){
+        clasePasivas.add(p);
+    }
+
+    public List<Pasiva> getClasePasivas() {
+        return clasePasivas;
     }
 }
--- src/main/java/com/personajesvideojuegos/modelo/Pasivass/clases/PaladinPasivas.java
+++ src/main/java/com/personajesvideojuegos/modelo/Pasivass/clases/PaladinPasivas.java
@@ -1,10 +1,16 @@
 package com.personajesvideojuegos.modelo.Pasivass.clases;
 
 import com.personajesvideojuegos.modelo.Pasivass.Pasiva;
-
+/**
+ * @author Alonso J rodríguez Betancor*/
 public class PaladinPasivas extends Pasiva {
     public PaladinPasivas(){
-        super("B1", "Proteccion Divina", "Se reduce el damage de los ataques magicos recibidos");
+        super("C", "Maestría del Paladin", "Aquí obtendras las pasivas del Paladin");
+        agregarclasePasiva(new Pasiva("C1", "Sentido Divino", "Capacidad de detectar no muertos, infernales, Celestiales"){});
+        agregarclasePasiva(new Pasiva("C2", "Imposición de manos", "Reserva de curación para ti como aliados"){});
+        //agregarclasePasiva(new Pasiva("C3", "Maestria de armas", "Obtienese la capacidad de ponder usar armas simples y marciales"){});
+        //agregarclasePasiva(new Pasiva("C4", "Maestria de armas", "Obtienese la capacidad de ponder usar armas simples y marciales"){});
+        //agregarclasePasiva(new Pasiva("C5", "Maestria de armas", "Obtienese la capacidad de ponder usar armas simples y marciales"){});
     }
 
 }
