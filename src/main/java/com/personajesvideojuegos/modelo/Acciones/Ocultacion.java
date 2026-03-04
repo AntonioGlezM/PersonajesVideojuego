2894d1a10df2729311df549a885c7517288d7bdd
Gabriel Francisco Ruíz Bolaños <gabrielfranciscosi45@gmail.com>
2026-03-04 15:06:11 +0000

    Cambios necesarios

--- src/main/java/com/personajesvideojuegos/modelo/Acciones/Ocultacion.java
+++ src/main/java/com/personajesvideojuegos/modelo/Acciones/Ocultacion.java
@@ -4,12 +4,13 @@ import com.personajesvideojuegos.modelo.Personaje;
 import com.personajesvideojuegos.modelo.Personajes.Picaro;
 
 public class Ocultacion extends Accion {
-    public Ocultacion(int destreza) {
-        super(destreza);
-    }
-    @Override
-    public void realizarAccion(Personaje objetivo) {
-        Picaro obj = (Picaro)objetivo;
-        obj.setValorArmadura(obj.getValorArmadura + obj.getDestreza());
-    }
+  public Ocultacion(int destreza) {
+    super(destreza);
+  }
+
+  @Override
+  public void realizarAccion(Personaje objetivo) {
+    Picaro obj = (Picaro) objetivo;
+    obj.setValorArmadura(obj.getValorArmadura + obj.getDestreza());
+  }
 }
